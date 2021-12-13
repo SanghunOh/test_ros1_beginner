07ca65564471b6fac161909ee6cd198f8e47827e
FIRST_NAME LAST_NAME <otter.oh@gmail.com>
2021-12-13 09:33:16 +0900

    first commit

---
+++ topic_first/nodes/example_pub.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+import rospy
+from std_msgs.msg import String
+
+def fun():
+    pass
+
+if __name__ == '__main__':
+    rospy.init_node('sample_pub')
+    pub = rospy.Publisher('hello', String, queue_size=10)
+
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        str = "hello publisher : %s " % rospy.get_time()
+        pub.publish(str)
+        rate.sleep()
+    pass
