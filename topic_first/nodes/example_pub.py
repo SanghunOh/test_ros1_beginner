a261fd82a4145e913798515bf4591c6c71bd5f7a
FIRST_NAME LAST_NAME <otter.oh@gmail.com>
2021-12-13 11:32:21 +0900

    pub <--> sub

--- topic_first/nodes/example_pub.py
+++ topic_first/nodes/example_pub.py
@@ -2,12 +2,16 @@
 import rospy
 from std_msgs.msg import String
 
-def fun():
+def fun_callback(msg):
+    rospy.loginfo('%s',msg.data)
     pass
 
 if __name__ == '__main__':
     rospy.init_node('sample_pub')
     pub = rospy.Publisher('hello', String, queue_size=10)
+
+    pub = rospy.Publisher('hello', String, queue_size=10)
+    rospy.Subscriber('hello02', String, callback=fun_callback)
 
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
--- topic_first/nodes/example_pub02.py
+++ topic_first/nodes/example_pub02.py
@@ -3,18 +3,21 @@
 import rospy
 from std_msgs.msg import String
 
+global pub
 def fun_callback(msg):
     rospy.loginfo('%s',msg.data)
 
-    pub = rospy.Publisher('hello02', String, queue_size=10)
-    pub.publish(msg.data)
+    global pub
+    str = "add publisher : %s " % rospy.get_time()
+    pub.publish(str)
 
     return
 
 if __name__ == '__main__':
     rospy.init_node('sample_sub')
-    rospy.Subscriber('hello', String, callback=fun_callback)
+    pub = rospy.Publisher('hello02', String, queue_size=10)
 
+    rospy.Subscriber('hello', String, callback=fun_callback)
     rospy.spin()
 
     pass
